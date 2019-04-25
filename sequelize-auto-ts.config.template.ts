e9fb8c3e8921b0ef08ebad1787ad47ffcd031090
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2019-04-25 16:03:41 +0800

    Added the template for the sequelize config.

---
+++ sequelize-auto-ts.config.template.ts
@@ -0,0 +1,9 @@
+export default {
+    /**
+     * Determines where the model files should be parsed from.
+     * 
+     * Note that this tool does not perform a recursive 
+     * transformation for each model file in subfolders.
+     */
+    "folderPath": "/folder/to/models/"
+}
