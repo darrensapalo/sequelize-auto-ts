a3162e16ae20e76f8897f7a4b8af02f52d4fe69f
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2019-04-25 19:19:13 +0800

    .gitignorefix

--- sequelize-auto-ts.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    /**
-     * Determines where the model files should be parsed from.
-     * 
-     * Note that this tool does not perform a recursive 
-     * transformation for each model file in subfolders.
-     */
-    "folderPath": "/Users/darrenkarlsapalo/git/sequelize-auto-ts/sample"
-}
