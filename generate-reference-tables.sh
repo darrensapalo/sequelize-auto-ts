2782f233255d4435db0adf11eaa813dd987ecd32
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2019-04-30 19:44:32 +0800

    Shell script to generate models and endpoints.

---
+++ generate-reference-tables.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+./generate-model-and-endpoint.sh AllergyType reference-tables/allergy-type
+./generate-model-and-endpoint.sh FamilyHistoryType reference-tables/family-history-type
+./generate-model-and-endpoint.sh hmo reference-tables/hmo
+./generate-model-and-endpoint.sh Hospital reference-tables/hospital
+./generate-model-and-endpoint.sh ImmunizationType reference-tables/immunization-type
+./generate-model-and-endpoint.sh LabRecordType reference-tables/lab-record-type
+./generate-model-and-endpoint.sh MedicalHistoryType reference-tables/medical-history-type
